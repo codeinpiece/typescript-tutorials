2464539299273a00407a27634a1a4529a5fc465e
codeinpiece <codeinpieces@gmail.com>
2023-02-15 22:30:20 +0500

    Commit for TypeScript Tutorials

---
+++ tutorials/variables/variables.js
@@ -0,0 +1,19 @@
+/*let a = 10;
+
+console.log(a);
+
+a = 89;
+
+console.log(a);
+
+a = a+1;
+
+console.log(a);
+
+let b = "Jhon"
+console.log(b);
+b = "Sara";
+
+console.log(b);*/
+var c = true;
+console.log(c);
---
+++ tutorials/variables/variables.ts
@@ -0,0 +1,21 @@
+/*let a = 10;
+
+console.log(a);
+
+a = 89;
+
+console.log(a);
+
+a = a+1;
+
+console.log(a);
+
+let b = "Jhon"
+console.log(b);
+b = "Sara";
+
+console.log(b);*/
+
+let c = true;
+
+console.log(c);
