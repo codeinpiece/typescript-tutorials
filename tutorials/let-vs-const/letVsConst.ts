81b6c49c54976d7aaa254adaff378dec717e8231
codeinpiece <codeinpieces@gmail.com>
2023-02-16 19:25:42 +0500

    let vs const keyword

---
+++ tutorials/let-vs-const/letVsConst.ts
@@ -0,0 +1,24 @@
+let num1 = 24;
+const num2 = 3;
+
+num1 = 26;
+
+num2 = 4;
+
+const pi = 3.141;
+
+let string1 = "Ali";
+
+const string2 = "Jhon";
+
+string2 = "Amjad";
+
+string1 = "Sara";
+
+
+let boolean1 = true;
+
+boolean1 = false;
+
+const boolean2 = false;
+boolean2 = true;
