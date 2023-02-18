9d7ecbae393ea7b03a3c25a8a7ba3102877ad024
codeinpiece <codeinpieces@gmail.com>
2023-02-18 10:16:32 +0500

    Code for Boolean Expressions (Relational Operators)

---
+++ tutorials/boolean-expressions/booleanExpressions.js
@@ -0,0 +1,13 @@
+var x = 6;
+var y = 10;
+/*let isGreaterThan = y>x;
+
+console.log(isGreaterThan);
+
+let isLessthan = x < y;
+console.log(isLessthan);*/
+/*
+let isGreaterThanorEqual = x >= y; //6 >= 10
+console.log(isGreaterThanorEqual);*/
+var isLessThanorEqual = x <= y;
+console.log(isLessThanorEqual);
---
+++ tutorials/boolean-expressions/booleanExpressions.ts
@@ -0,0 +1,16 @@
+let x = 6;
+let y = 10;
+
+let isGreaterThan = y>x;
+
+console.log(isGreaterThan);
+
+let isLessthan = x < y;
+console.log(isLessthan);
+
+
+let isGreaterThanorEqual = x >= y; //6 >= 10
+console.log(isGreaterThanorEqual);
+
+let isLessThanorEqual = x<=y; // 6<10 , 6 = 10
+console.log(isLessThanorEqual);
