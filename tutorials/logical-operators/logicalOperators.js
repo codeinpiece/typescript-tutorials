5cd654319395ad35111f495b2250a309ac4626d3
codeinpiece <codeinpieces@gmail.com>
2023-02-26 20:50:58 +0500

    Logical Operators

---
+++ tutorials/logical-operators/logicalOperators.js
@@ -0,0 +1,11 @@
+var x = 10;
+var y = 20;
+// AND Operator
+var resultAND = (x > 15) && (y > 15); // false AND true = false
+//console.log(resultAND);
+// OR logical Operator
+var resultOR = (x > 15) || (y > 15); //false OR true = true 
+//console.log(resultOR);
+//NOT Operator 
+var resultNOT = (!resultOR);
+console.log(resultNOT);
---
+++ tutorials/logical-operators/logicalOperators.ts
@@ -0,0 +1,14 @@
+let x = 10;
+let y =20;
+
+// AND Operator
+let resultAND = (x>15) && (y >15); // false AND true = false
+//console.log(resultAND);
+
+// OR logical Operator
+let resultOR = (x>15) || (y>15); //false OR true = true 
+//console.log(resultOR);
+
+//NOT Operator 
+let resultNOT = (!resultOR);
+console.log(resultNOT);
