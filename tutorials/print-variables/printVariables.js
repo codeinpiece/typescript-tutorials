5e607a03c161c2cee44a4df8fad395f1da3ff9bf
codeinpiece <codeinpieces@gmail.com>
2023-02-26 21:55:15 +0500

    Print Variables *Dynamic Strings)

---
+++ tutorials/print-variables/printVariables.js
@@ -0,0 +1,5 @@
+var myName = "Alice";
+var age = 30;
+console.log(age);
+// My name is Alice and I am 30 years old.
+console.log("My name is ".concat(myName, " and I am ").concat(age, " years old"));
---
+++ tutorials/print-variables/printVariables.ts
@@ -0,0 +1,8 @@
+let myName = "Alice";
+let age = 30;
+
+console.log(age);
+
+// My name is Alice and I am 30 years old.
+
+console.log(`My name is ${myName} and I am ${age} years old`);
