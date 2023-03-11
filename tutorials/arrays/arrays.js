c51b8e50299f88c2d058b6ef18dc4cd0259f1d2c
codeinpiece <codeinpieces@gmail.com>
2023-03-11 23:19:29 +0500

    Arrays in TS

---
+++ tutorials/arrays/arrays.js
@@ -0,0 +1,7 @@
+var numbers = [1, 2, 3, 4, 5];
+// Access the 3rd element
+console.log(numbers[2]);
+var names = ['Alice', 'John', 'William'];
+console.log(names[0]);
+var resultAdd = numbers[0] + numbers[3]; //5
+console.log(resultAdd);
---
+++ tutorials/arrays/arrays.ts
@@ -0,0 +1,13 @@
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+// Access the 3rd element
+
+console.log(numbers[2]);
+
+let names: string[] = [ 'Alice', 'John', 'William'];
+
+console.log(names[0]);
+
+let resultAdd = numbers[0] + numbers[3]; //5
+
+console.log(resultAdd)
