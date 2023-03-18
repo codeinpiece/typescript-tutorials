631661c7158f41afbf6c92703ee1660e41e0775c
codeinpiece <codeinpieces@gmail.com>
2023-03-19 02:33:10 +0500

    Beginner Web App

--- tutorials/webapp_beginners/app.ts
+++ tutorials/webapp_beginners/app.ts
@@ -1,3 +1,9 @@
+// GitHub Repo: https://github.com/codeinpiece/typescript-tutorials
+// YouTube Channel: https://www.youtube.com/@codeinpieces
+// YouTube Video Link: https://youtu.be/J2kUg78Lf4o
+
+// Topic: Beginners Understanding on Web Apps using HTML and typescript.
+
 const number1 = document.getElementById("num1") as HTMLInputElement
 const printButton = document.getElementById("printBtn") as HTMLButtonElement
 const printValue = document.getElementById("enteredNumber") as HTMLOutputElement
