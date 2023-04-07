b38f038144605ae0e8c2fe4bb6b1fd6b3c027ba1
codeinpiece <codeinpieces@gmail.com>
2023-04-07 17:11:34 +0500

    Inquirer based CLI Calculator

---
+++ tutorials/inquirer-calculator/index.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'num1',
+        message: 'Enter the first number: '
+    },
+    {
+        type: 'number',
+        name: 'num2',
+        message: 'Enter the second number: '
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: 'Select the operator you want to perform:',
+        choices: ['Add', 'Subtract', 'Multiply', 'Divide']
+    }
+]);
+
+let result: number;
+
+switch(answer.operator){
+    case 'Add':
+        result = answer.num1 + answer.num2;
+        console.log("The answer for addition is: " + result);
+        break;
+    
+    case 'Subtract':
+        result = answer.num1 - answer.num2;
+        console.log("The answer for subtraction is: " + result);
+        break;
+
+    case 'Multiply':
+        result = answer.num1 * answer.num2;
+        console.log("The answer for multiplication is: " + result);
+        break;
+
+    case 'Divide':
+        result = answer.num1 / answer.num2;
+        console.log(" The answer for division is: " + result);
+        break;
+}
